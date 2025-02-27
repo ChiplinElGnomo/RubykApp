7b9c3a63167739ed1084d9859e3c37454838653f
Spidy <chiplinelgnomo@gmail.com>
2025-02-27 11:23:51 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Rubyk;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
